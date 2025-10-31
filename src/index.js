263d14de48b9255d255c89501caf4578cb09ccf1
Lucas Correia <lucas.carvalho79@hotmail.com>
2025-10-30 22:39:38 -0300

    test: adicionar testes de integração

--- src/index.js
+++ src/index.js
@@ -1,32 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const YAML = require('yamljs');
-const swaggerUi = require('swagger-ui-express');
-
-const authRoutes = require('./routes/authRoutes');
-const servicosRoutes = require('./routes/servicosRoutes');
-const compromissosRoutes = require('./routes/compromissosRoutes');
-const cabeleireirosRoutes = require('./routes/cabeleireirosRoutes');
-
-const swaggerDocument = YAML.load('./resources/swagger.yaml');
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use('/auth', authRoutes);
-app.use('/servicos', servicosRoutes);
-app.use('/compromissos', compromissosRoutes);
-app.use('/cabeleireiros', cabeleireirosRoutes);
-
-// Rota para visualizar a documentação Swagger
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// rota raiz
-app.get('/', (req, res) => {
-  res.json({ mensagem: 'API do Salão de Beleza — bem vindo(a)! Visite /docs para a documentação Swagger.' });
-});
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
